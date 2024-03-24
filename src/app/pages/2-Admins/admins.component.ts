d4662d93ccb74ca1d27b9646006c1a484542c0b2
Caroline <carolinekaram2020@gmail.com>
2024-03-24 19:54:35 +0200

    Freelancers & Admins Table

--- src/app/pages/2-Admins/admins.component.ts
+++ src/app/pages/2-Admins/admins.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IUser } from '../../interfaces/user';
+import { UsersService } from '../../services/services/user.service';
 
 @Component({
   selector: 'app-admins',
@@ -8,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './admins.component.css'
 })
 export class AdminsComponent {
+  admins : IUser[] = [];
+  constructor(
+    private adminService: UsersService
+  ){}
+  ngOnInit(): void {
 
+  //  all services
+    this.adminService.getallAdmins().subscribe((data:any) => {
+      this.admins = data.admins;
+      console.log(this.admins);
+
+    });
+  };
+    deleteUser(id: string) {
+      this.adminService.deleteUser(id).subscribe((data) => {
+       console.log(data);
+
+      });
+  }
 }
